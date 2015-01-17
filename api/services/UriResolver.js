72d55af4400397981acc209f3d363509e08b0ca9
rogerhokp <rogerhokp@gmail.com>
2015-01-17 18:25:57 +0800

    Rename config RuleMapping.js to mapping.js

--- api/services/UriResolver.js
+++ api/services/UriResolver.js
@@ -1,4 +1,4 @@
-var mapping = require('../rules/RuleMapping.js');
+var mapping = require('../rules/mapping.js');
 module.exports = function (url) {
     for (rule in mapping) {
         if (url.match(mapping[rule])) {
